d8479336775c3c717bb7f4a9540b7ce013138990
ATB <parvatimore1710@gmail.com>
2024-07-18 12:25:45 +0530

    Merge remote-tracking branch 'origin/master'

--- src/ex1507/lab002.java
+++ src/ex1507/lab002.java
@@ -2,6 +2,8 @@ package ex1507;
 
 public class lab002 {
     public static void main(String[] args) {
+        System.out.println("Practice  edit lab 2 file to commit file");
+        // 2+2    - Single comment
 
     }
 }
