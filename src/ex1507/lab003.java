0661ff6bd7eb90f631b03b4ec9d155fbfc43a882
ATB <parvatimore1710@gmail.com>
2024-07-18 13:29:09 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/ex1507/lab002.java

--- src/ex1507/lab003.java
+++ src/ex1507/lab003.java
@@ -1,4 +1,26 @@
 package ex1507;
 
 public class lab003 {
-}
+    public static void main(String[] args) {
+
+                // Single Comment - This code will not run or executed
+                //  2+2 ?
+
+                // Purpose Comment ?
+                // Instructions to Jr QA or Dev
+                // explain
+
+                /**
+                 * Multiple Comment
+                 * Documentation Purpose
+                 *
+                 *
+                 *
+                 */
+
+
+
+
+            }
+    }
+
